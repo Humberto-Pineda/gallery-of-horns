def1bd1fc4f7459050ffc426ea994821c8ebea3d
Humberto Pineda <bpineda7540@gmail.com>
2022-08-16 14:33:11 -0700

    finished stretch goal for class 1

--- src/Beast.js
+++ src/Beast.js
@@ -2,11 +2,12 @@ import React from "react";
 
 class Beast extends React.Component {
   render() {
-    console.log(this.props.title);
+    // console.log(this.props.title);
+    console.log(this.props.image_url);
     return (
       <article>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imgUrl}  alt={this.props.altImg}/>
+        <img src={this.props.image_url}  alt={this.props.altImg}/>
         <p>{this.props.description}</p>
       </article>
     )
